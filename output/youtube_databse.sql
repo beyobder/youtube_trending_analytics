d79c0d1a59a880ef6639440d886c375ab8cfc215
beyobder <beyobder@gmail.com>
2026-08-29 10:38:25 +0800

    Add youtube_database.sql to output

---
+++ output/youtube_databse.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict ArB9yZXTGoBObCLJlt0gcdIWLxCM1XyqPscM5fGBQcGSn4odgGno2hp1nsLN61T
+
+-- Dumped from database version 18.6
+-- Dumped by pg_dump version 18.6
+
+-- Started on 2026-08-29 10:20:26
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2026-08-29 10:20:26
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict ArB9yZXTGoBObCLJlt0gcdIWLxCM1XyqPscM5fGBQcGSn4odgGno2hp1nsLN61T
+
